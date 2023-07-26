23d29490a19dd0f96dca5520eff88d181ea31d6d
antonov1lya <a222i@ya.ru>
2023-07-26 12:17:23 +0300

    some fixes

--- src/gaussian.py
+++ src/gaussian.py
@@ -6,8 +6,8 @@ from .numerical_characteristics import pearson
 def pearson_test(x: np.ndarray, threshold: float) -> np.ndarray:
     """
     Calculates p-values for testing N(N-1)/2 hypotheses of the form:
-    H_ij: The Pearson measure of similarity between the i and j component 
-    of the random vector <= threshold vs K_ij: The Pearson measure of similarity 
+    H_ij: The Pearson measure of similarity between the i and j component
+    of the random vector <= threshold vs K_ij: The Pearson measure of similarity
     between the i and j component of the random vector > threshold.
 
     Parameters
@@ -22,7 +22,7 @@ def pearson_test(x: np.ndarray, threshold: float) -> np.ndarray:
     -------
     p_value : (N,N) ndarray
         Matrix of p-values.
-    
+
     """
     n, N = x.shape
 
@@ -30,11 +30,14 @@ def pearson_test(x: np.ndarray, threshold: float) -> np.ndarray:
         return 0.5 * np.log((1 + y) / (1 - y))
 
     def statistics(y):
+        if y == 1:
+            return np.inf
+        if y == -1:
+            return -np.inf
         return np.sqrt(n) * (z_transform(y) - z_transform(threshold))
 
-    def calc_p_value(y):
-        return 0 if y == 1 else 1 if y == -1 else 1 - norm.cdf(statistics(y))
+    def calc_p_values(y):
+        return 1 - norm.cdf(statistics(y))
 
-    transformer = np.vectorize(calc_p_value)
-    p_value = transformer(pearson(x))
-    return p_value
+    transformer = np.vectorize(calc_p_values)
+    return transformer(pearson(x))
