4c46b8c33ea8b194191aff74ce0c4bd93d9c3184
antonov1lya <a222i@ya.ru>
2023-08-24 17:21:20 +0300

    add pcc files

--- src/_build_pyx.py
+++ src/_build_pyx.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules=cythonize("segment_tree.pyx"),
+    ext_modules=cythonize("_segment_tree.pyx"),
     include_dirs=[numpy.get_include()]
 )
